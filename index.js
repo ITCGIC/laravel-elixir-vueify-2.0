ed5b9e9044240bb998a05480a86f8cff35494377
sreng-step <srengty@gmail.com>
2022-01-28 23:29:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Elixir = require('laravel-elixir');
+if(Elixir.config.js.browserify.transformers){
+    Elixir.config.js.browserify.transformers.push({
+        name: 'vueify',
 
-Elixir.config.js.browserify.transformers.push({
-    name: 'vueify',
-
-    // https://github.com/vuejs/vueify#usage
-    options: {}
-});
+        // https://github.com/vuejs/vueify#usage
+        options: {}
+    });
+}
